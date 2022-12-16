2eb75dc2efba863dc3614225be7e45fc6c050c0b
Guilherme Ferrari <guile.ferrari@hotmail.com>
2022-12-16 14:23:10 -0300

    project: initial commit

---
+++ HashPasswords/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+var tempoDecorrido = Stopwatch.StartNew();
+
+var senhas = new HashSet<string>()
+{
+    "1aa41cab2321711523f6daa34e009492",
+    "ad61dfd4bbd9432941292a3927668776",
+    "7497e482b8d38647c7806487cf6481d4",
+    "2ce9aff916492d784087bfbac1aa9034",
+    "dcb7d034362b6c6630cdad79d00ad15a"
+};
+
+string[] linhasDicionarioDeSenhas = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "rockyou.txt"));
+
+Console.WriteLine($"Linhas lidas do arquivo: {linhasDicionarioDeSenhas.Count()}");
+
+foreach (var linhaDicionario in linhasDicionarioDeSenhas)
+{
+    string removerSenha = string.Empty;
+
+    foreach (var senha in senhas)
+    {
+        if (senha.ToUpper().Equals(RealizarHashMD5(linhaDicionario)))
+        {
+            removerSenha = senha;
+            Console.WriteLine($"{senha} - {linhaDicionario}");
+        }
+
+        if (!string.IsNullOrEmpty(removerSenha))
+            senhas.Remove(removerSenha);
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Tempo decorrido: {tempoDecorrido.Elapsed.TotalSeconds}");
+
+string RealizarHashMD5(string senha)
+{
+    StringBuilder stringBuilder = new StringBuilder();
+    foreach (var letra in MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(senha)))
+    {
+        stringBuilder.Append(letra.ToString("X2"));
+    }
+
+    return stringBuilder.ToString();
+}
---
+++ TimeHash/Program.cs
@@ -0,0 +1,91 @@
+﻿using Isopoh.Cryptography.Argon2;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+double tempo = 15;
+
+var senha = "Sup3erS3cret@!";
+
+double segundos = 0;
+int total = 0;
+
+Console.WriteLine($"Quantas vezes {senha} podem ser feito hash em {tempo} segundos");
+
+Console.WriteLine();
+Console.WriteLine("________________________________________________________________");
+Console.WriteLine();
+
+(segundos, total) = RealizarHashMD5(senha);
+Console.WriteLine($"Hash MD5: Tempo: {segundos}, Vezes: {total:N}");
+Console.WriteLine();
+
+(segundos, total) = RealizarHashSHA256(senha);
+Console.WriteLine($"Hash SHA256: Tempo: {segundos}, Vezes: {total:N}");
+Console.WriteLine();
+
+(segundos, total) = RealizarHashArgon2(senha);
+Console.WriteLine($"Hash Argon2: Tempo: {segundos}, Vezes: {total:N}");
+Console.WriteLine();
+
+(double, int) RealizarHashMD5(string senha)
+{
+    int total = 0;
+    var segundos = Stopwatch.StartNew();
+    
+    do
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (var letra in MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(senha)))
+        {
+            stringBuilder.Append(letra.ToString("X2"));
+        }
+
+        total++;
+    } while (segundos.Elapsed.TotalSeconds <= tempo);
+
+    segundos.Stop();
+
+    return (segundos.Elapsed.TotalSeconds, total);
+}
+
+(double, int) RealizarHashSHA256(string senha)
+{
+    int total = 0;
+    var segundos = Stopwatch.StartNew();
+
+    do
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(senha));
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            total++;
+        }
+    } while (segundos.Elapsed.TotalSeconds <= tempo);
+
+    segundos.Stop();
+
+    return (segundos.Elapsed.TotalSeconds, total);
+}
+
+(double, int) RealizarHashArgon2(string senha)
+{
+    int total = 0;
+    var segundos = Stopwatch.StartNew();
+
+    do
+    {
+        var value = Argon2.Hash(senha);
+        total++;
+    } while (segundos.Elapsed.TotalSeconds <= tempo);
+
+    segundos.Stop();
+
+    return (segundos.Elapsed.TotalSeconds, total);
+}
